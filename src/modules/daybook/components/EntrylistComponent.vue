c7fc316a5e6d3006d89bbeb072fa166b2d5302d7
Alangobe <alangobe01@gmail.com>
2022-06-30 13:04:57 -0500

    Creado entry component y modificados componentes

--- src/modules/daybook/components/EntrylistComponent.vue
+++ src/modules/daybook/components/EntrylistComponent.vue
@@ -1,10 +1,37 @@
 <template>
-  <h1>Entry list</h1>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input type="text" class="form-control" placeholder="Buscar entrada" />
+    </div>
+    <div class="entry-scrollarea">
+      <EntryComponent :key="item" v-for="item in 100" />
+    </div>
+  </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
 export default {
-
-}
+  components: {
+    EntryComponent: defineAsyncComponent(() => import('./EntryComponent.vue') )
+  }
+};
 </script>
 
+<style lang="scss" scoped>
+
+  input {
+    height: 40px;
+  }
+
+  .entry-list-container {
+    border-right: 1px solid #2c3e50;
+    height: calc(100vh - 56px);
+  }
+
+  .entry-scrollarea {
+    height: calc(100vh - 120px);
+    overflow: scroll;
+  }
+
+</style>
