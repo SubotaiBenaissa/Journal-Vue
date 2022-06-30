13a194d7a941835f16249c7ffdf6e6bb3a30ef3a
Alangobe <alangobe01@gmail.com>
2022-06-29 21:05:21 -0500

    Añadido sass al proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>XD</h1>
+    <button class="btn btn-primary">primero wtf</button>
+    <button class="btn btn-secondary">segundo .-.</button>
+    <button class="btn btn-success">succ sess</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
