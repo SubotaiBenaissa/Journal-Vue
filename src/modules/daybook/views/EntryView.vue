f677a6ec59f09c9a36274baeec23bdde0ac04381
Alangobe <alangobe01@gmail.com>
2022-07-04 22:31:31 -0500

    Creados estilos para componente Entry

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -18,7 +18,8 @@
   <div class="d-flex flex-column px-3 h-75">
     <textarea placeholder="OwO"> </textarea>
   </div>
-  <Fab/>
+  <Fab icon="fa-save"/>
+  <img src="https://pm1.narvii.com/6580/57583a554a9e2bcfd6d0bc5c9897c2aed67ec1ad_hq.jpg" alt="entry-picture" class="img-thumbnail">
 </template>
 
 <script>
@@ -32,6 +33,23 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+  textarea {
+    font-size: 20px;
+    border: none;
+    height: 100%;
+    &:focus {
+      outline: none;
+    }
+  }
+
+  img {
+    width: 200px;
+    position: fixed;
+    bottom: 150px;
+    right: 20px;
+    box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+  }
 
 </style>
