2fcff26e142548af65cf40bc3ccbae6455f422ba
Alangobe <alangobe01@gmail.com>
2022-07-12 18:30:42 -0500

    Estado

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,5 +1,24 @@
 
 export default () => ({
     isLoading: true,
-    entries: 0
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum XD',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 100,
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum segundo XD',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 110,
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum tercero XD',
+            picture: null,
+        },
+    ]
 })
